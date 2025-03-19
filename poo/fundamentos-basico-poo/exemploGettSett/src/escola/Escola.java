c69e4ec89f97022ba5c52332f46774e6ae7a6e41
rafinha2508 <rafaekely51@gmail.com>
2025-03-18 21:45:09 -0300

    refactor: Classe aderente ao Java Beans

--- poo/fundamentos-basico-poo/exemploGettSett/src/escola/Escola.java
+++ poo/fundamentos-basico-poo/exemploGettSett/src/escola/Escola.java
@@ -3,9 +3,9 @@ package escola;
 public class Escola {
     public static void main(String[] args) {
         Aluno felipe = new Aluno();
-        felipe.nome = "Felipe";
-        felipe.idade = 18;
+        felipe.setNome("Felipe");
+        felipe.setIdade(10);
 
-        System.out.println("o aluno " + felipe.nome + " tem idade: " + felipe.idade + " anos");
+        System.out.println("o aluno "+ felipe.getNome() + " tem " + felipe.getIdade() + " anos");
     }
 }
